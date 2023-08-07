efcba34991d466a4ff7e4d5883b0d6b437ae99a2
Marcos Antonio <marcos.antoniodm039@gmail.com>
2023-08-06 22:08:53 -0300

    feat: implement repository support

--- app/Repositories/SupportEloquentORM.php
+++ app/Repositories/SupportEloquentORM.php
@@ -2,21 +2,42 @@
 
 use App\DTO\CreateSupportDTO;
 use App\DTO\UpdateSupportDTO;
+use App\Models\Support;
 use App\Repositories\SupportRepositoryInterface;
 
 class SupportEloquentORM implements SupportRepositoryInterface
 {
+
+    public function __construct(
+        protected Support $model,
+    ) {}
     public function getAll(string $filter = null): array
     {
-
+        return $this->model
+                    ->where(function ($query) use ($filter) {
+                        if ($filter) {
+                            /**
+                             * O SQL ABAIXO É EQUIVALENTE A:
+                             * SELECT * FROM supports WHERE subject = $filter OR body LIKE %$filter%
+                             */
+                            $query->where('subject', $filter);
+                            $query->orWhere('body', 'like', "%{$filter}%");
+                        }
+                    })
+                    ->all()
+                    ->toArray();
     }
     public function findOne(string $id): stdClass|null
     {
-
+        $support = $this->model->find($id);
+        if(!$support) {
+            return null;
+        }
+        return (object) $support->toArray();
     }
     public function delete(string $id): void
     {
-
+        $this->model->findOrFail($id)->delete();
     }
     public function new(CreateSupportDTO $dto): stdClass
     {
