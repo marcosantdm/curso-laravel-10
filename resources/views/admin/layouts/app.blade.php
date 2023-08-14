fea3b85de2e452b0e5d7db0e562811bbc4531550
Marcos Antonio <marcos.antoniodm039@gmail.com>
2023-08-14 18:13:27 -0300

    refactor: use template and create header

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,15 +8,21 @@
     <title>@yield('title') - {{ config('app.name') }}</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
+
 <body>
-    <head>
-        @yield('header')
-    </head>
-    <div class="content">
-        @yield('content')
-    </div>
-    <footer>
-        #default footer
-    </footer>
+    <section class="container px-4 mx-auto">
+        <div class="sm:flex sm:items-center sm:justify-between">
+            @yield('header')
+        </div>
+
+        <div class="content">
+            @yield('content')
+        </div>
+
+        <footer>
+            #default footer
+        </footer>
+    </section>
 </body>
+
 </html>
