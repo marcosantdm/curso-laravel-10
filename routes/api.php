051e1cccdba3f3bf87f0243187310b844d1ba520
Marcos Antonio <marcos.antoniodm039@gmail.com>
2023-08-15 18:38:43 -0300

    feat: auth api with sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,19 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\AuthController;
 use App\Http\Controllers\Api\SupportController;
 use Illuminate\Support\Facades\Route;
+
+Route::post('/login', [AuthController::class, 'auth']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('/me', [AuthController::class, 'me'])->middleware(['auth:sanctum']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::apiResource('/supports', SupportController::class);
+});
 
 /**
  * Ao criar essa rota, automaticamente estaremos criando as outras para os métodos, reduzindo o número de linhas
  * Exemplo: Rota para store, show, delete e put
  */
-
-Route::apiResource('/supports', SupportController::class);
+// Route::apiResource('/supports', SupportController::class)->middleware(['auth:sanctum']);
