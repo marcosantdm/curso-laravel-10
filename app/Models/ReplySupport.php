528e65a2980e47ea416f832c3bb3d70042f78ca8
Marcos Antonio <marcos.antoniodm039@gmail.com>
2023-08-24 13:29:35 -0300

    feat: create accessor for date

--- app/Models/ReplySupport.php
+++ app/Models/ReplySupport.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -17,6 +19,13 @@ class ReplySupport extends Model
         'support_id',
         'user_id',
     ];
+
+    public function createdAt(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $createdAt) => Carbon::make($createdAt)->format('d/m/Y H:i')
+        );
+    }
     protected $table = 'replies_supports';
 
     public function user(): BelongsTo
