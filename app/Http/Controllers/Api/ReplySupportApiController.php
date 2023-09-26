34fe2aa98fbd30c2147a958bb14423e1f88fb774
marcosantdm <marcos.antoniodm039@gmail.com>
2023-09-25 21:44:36 -0300

    feat: create controller api list replies of support

---
+++ app/Http/Controllers/Api/ReplySupportApiController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ReplySupportResource;
+use App\Services\ReplySupportService;
+use App\Services\SupportService;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class ReplySupportApiController extends Controller
+{
+    public function __construct(
+        protected SupportService $supportService,
+        protected ReplySupportService $replyService,
+    ) {
+    }
+
+    public function getRepliesFromSupport(string $supportId)
+    {
+        /**
+         ** Support:find($supportId) busca pela primary key, nesse caso o ID
+         ** Support:where('id', $supportId) busca pelo campo especificado, nesse caso o ID
+         ** Support:where('id', $supportId)->first() busca pelo campo especificado, nesse caso o ID, e retorna apenas o primeiro registro
+         ** Support:where('id', $supportId)->get() busca pelo campo especificado, nesse caso o ID, e retorna todos os registros
+         ** Support:where('id', $supportId)->paginate(10) busca pelo campo especificado, nesse caso o ID, e retorna todos os registros com paginação
+         ** Support:where('id', '=', $supportId) busca pelo campo especificado, nesse caso o ID, e retorna os registros utilizando o operador de comparação
+         */
+
+        if (!$this->supportService->findOne($supportId)) {
+            return response()->json([
+                'message' => 'Não encontrado',
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        $replies = $this->replyService->getAllBySupportId($supportId);
+
+        return ReplySupportResource::collection($replies);
+    }
+
+}
